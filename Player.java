75c4720720fac97028d5ff2561421828f46bab67
saysah <sayf.sahaf@gmail.com>
2024-11-22 05:47:09 -0500

    get most things to work. delete function is a bit wonky. need to look into it.

---
+++ Player.java
@@ -0,0 +1,25 @@
+public class Player {
+
+    int playerID;
+    String playerName;
+    int playerAge;
+    int playerJersey;
+    String playerPosition;
+    String nationality;
+
+    public Player(
+        int playerID,
+        String playerName,
+        int playerAge,
+        int playerJersey,
+        String playerPosition,
+        String nationality
+    ) {
+        this.playerID = playerID;
+        this.playerName = playerName;
+        this.playerAge = playerAge;
+        this.playerJersey = playerJersey;
+        this.playerPosition = playerPosition;
+        this.nationality = nationality;
+    }
+}
